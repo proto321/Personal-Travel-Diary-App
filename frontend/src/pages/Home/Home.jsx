cd89b0afd07384c6eaf467b9e721bc528aa44f24
Harsh Sahu <prototype.myid@gmail.com>
2025-11-25 19:20:19 +0530

    React-router-dom installation setup

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 //rafce
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
@@ -7,4 +7,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
