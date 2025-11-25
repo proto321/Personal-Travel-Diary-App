90d1640157034bb9d1443a5f47e0ea6b281ce560
Harsh Sahu <prototype.myid@gmail.com>
2025-11-25 19:18:01 +0530

    React-router-dom installation and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-
 // rafce
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 
 const App = () => {
   return (
-    <div className='text-pink-500 text-4xl'>
-      
-      <button>
-      App
-      </button>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' exact element={<Home />} /> 
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/sign-up' exact element={<SignUp />} />
+        
+        
+
+      </Routes>
+      </BrowserRouter>
+    </>
 
   )
 }
