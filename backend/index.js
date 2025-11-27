3f6c59bf614e7c03a583f2e0bc4241f94d3053ed
Harsh Sahu <prototype.myid@gmail.com>
2025-11-27 18:40:39 +0530

    connected to database

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+const app = express();
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+
+app.listen(3000);
+console.log('Server is running on http://localhost:3000');
