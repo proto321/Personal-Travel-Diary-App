66cf67c14cb3a49b560e5ddf92bd14aa3cc9bdf0
Harsh Sahu <prototype.myid@gmail.com>
2026-01-18 18:54:54 +0530

    Connected to Database

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username ||
+        !email ||
+        !password ||
+        username === "" ||
+        email === "" ||
+        password === ""
+        ) {
+
+            // message will show if any of the deltails are missing
+        return res.status(400).json({ message: "Fill all required details." });
+    }
+    
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+
+        username,
+        email,
+        password: hashedPassword,
+    })
+
+    try {
+        await newUser.save(); 
+        res.json("Sign successful")
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 const app = express();
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI)
@@ -13,6 +15,10 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('Error connecting to MongoDB', err);
   });
 
+  //for allowing json data in request body
+app.use(express.json());
 
 app.listen(3000);
 console.log('Server is running on http://localhost:3000');
+
+app.use("/api/auth", authRoutes);
