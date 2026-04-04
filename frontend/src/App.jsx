228162d907f416044b12c12acfd34f11f64e89da
Harsh Sahu <prototype.myid@gmail.com>
2026-04-04 19:53:03 +0530

    create signup api route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,6 @@ const App = () => {
         <Route path='/' exact element={<Home />} /> 
         <Route path='/login' exact element={<Login />} />
         <Route path='/sign-up' exact element={<SignUp />} />
-        
-        
 
       </Routes>
       </BrowserRouter>
--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -2,8 +2,10 @@
 import React from 'react'
 
 const SignUp = () => {
-  return (
+  return ( <>
     <div>SignUp</div>
+    
+    </>
   )
 }
 
