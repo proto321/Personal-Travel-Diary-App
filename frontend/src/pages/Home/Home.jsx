8673b58ef119e4d1b6d66359dc3c09a544103125
Harsh Sahu <prototype.myid@gmail.com>
2026-05-25 08:40:15 +0530

    Get all travel story api integration

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,44 @@
 //rafce
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from '../../components/Navbar';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Home = () => {
+    const [allStories, setAllstories] = useState([])
+
+    console.log(allStories)
+
+  // Get all travel stories
+  const getAllTravelStories = async () => {
+    try{
+      const response = await axiosInstance.get('/travel-story/get-all') 
+
+      if (response.data && response.data.stories) {
+        // setAllstories(response.data.stories);
+        setAllstories(response.data.stories);
+
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again.");
+    }
+  }
+
+  useEffect(() => {
+    getAllTravelStories()
+
+  return () => {}
+}, [])
+  
   return <>
   <Navbar />
+
+  <div className='container mx-auto py-10'>
+    <div className='flex gap-7'>
+      <div className='flex-1'></div>
+
+      <div className='w-[320px]'></div>
+    </div>
+  </div>
   </>
 }
 
