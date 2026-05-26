e5f4ae40eb32e334e9aa9f9ca5bf2a2352290953
Harsh Sahu <prototype.myid@gmail.com>
2026-05-27 00:25:04 +0530

    Travel story card components

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 //rafce
 import React, { useState, useEffect } from 'react';
-import { Navbar } from '../../components/Navbar';
+import { Navbar }  from '../../components/Navbar';
 import axiosInstance from '../../utils/axiosInstance';
+import TravelStoryCard from '../../components/TravelStoryCard';
 
 const Home = () => {
     const [allStories, setAllstories] = useState([])
@@ -9,19 +10,40 @@ const Home = () => {
     console.log(allStories)
 
   // Get all travel stories
+  // const getAllTravelStories = async () => {
+  //   try{
+  //     const response = await axiosInstance.get('/travel-story/get-all') 
+
+  //     if (response.data && response.data.stories) {
+  //       // setAllstories(response.data.stories);
+  //       setAllstories(response.data.stories);
+
+  //     }
+  //   } catch (error) {
+  //     console.log("Something went wrong. Please try again.");
+  //   }
+  // }
   const getAllTravelStories = async () => {
-    try{
-      const response = await axiosInstance.get('/travel-story/get-all') 
-
-      if (response.data && response.data.stories) {
-        // setAllstories(response.data.stories);
-        setAllstories(response.data.stories);
-
+    try {
+      const response = await axiosInstance.get('/travel-story/get-all');
+  
+      console.log("API Response:", response.data);
+  
+      if (response.data) {
+        setAllstories(response.data);
       }
+  
     } catch (error) {
-      console.error("Something went wrong. Please try again.");
+      console.log("Something went wrong. Please try again.");
     }
   }
+
+  //Handle Edit Story
+  const handleEdit = async (data) => {  }
+
+  const handleViewStory = (data) => {  }
+
+  const updateIsFavorite = async (data) => { }
 
   useEffect(() => {
     getAllTravelStories()
@@ -34,7 +56,29 @@ const Home = () => {
 
   <div className='container mx-auto py-10'>
     <div className='flex gap-7'>
-      <div className='flex-1'></div>
+      <div className='flex-1'>
+        {allStories.length > 0 ? (
+          <div className='grid grid-cols-2'>
+          {allStories.map((item) => {
+            return (
+              <TravelStoryCard key={item._id} 
+              imageUrl={item.imageUrl} 
+              title={item.title} 
+              story={item.story} 
+              date={item.visitedDate}
+              visitedLocation={item.visitedLocation}
+              isFavourite={item.isFavourite}
+              onEdit={() => handleEdit(item)}
+              onClick={() => handleViewStory(item)}
+              onFavoriteClick={() => updateIsFavorite(item)}
+              />
+            )
+          })}
+          </div>
+        ) : (
+          <div>Empty Cards Here</div>
+        )}
+      </div>
 
       <div className='w-[320px]'></div>
     </div>
